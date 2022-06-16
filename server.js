ae7e9c0e21d5382fbdadc77226976a33fff27d69
maddnty <nzkb24@gmail.com>
2022-06-16 17:10:31 -0500

    added get all departments route

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 const express = require("express");
-const db = require("./db/connection");
+const apiRoutes = require('./routes/apiRoutes')
+const db = require('./db/connection');
 
 const PORT = process.env.PORT || 3002;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use('/api', apiRoutes);
 
 app.use((req, res) => {
   res.status(400).end();
