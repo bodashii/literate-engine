dd4ba74198d6f1fcbc6e64d5036834f3eee4c616
maddnty <nzkb24@gmail.com>
2022-06-16 16:06:01 -0500

    server connected

---
+++ server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require("express");
+const db = require("./db/connection");
+
+const PORT = process.env.PORT || 3002;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use((req, res) => {
+  res.status(400).end();
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log("Database connected.");
+  app.listen(PORT, () => {
+    console.log(`Server is up and running on ${PORT}`);
+  });
+});
